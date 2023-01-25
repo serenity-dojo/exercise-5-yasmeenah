bc85f1b6d96a8dae9dc0711ddf0db10bcc9221c5
yasmeenah <84631193+yasmeenah@users.noreply.github.com>
2023-01-26 00:39:23 +0200

    exersice 7 done

--- src/main/java/com/serenitydojo/Cat.java
+++ src/main/java/com/serenitydojo/Cat.java
@@ -1,22 +1,16 @@
 package com.serenitydojo;
 
-public class Cat {
-    private String name;
+public class Cat extends Pet {
     private String favoriteToy;
-    private int age;
 
     public Cat(String name, String favoriteToy, int age) {
-        this.name = name;
+        super(name, age);
         this.favoriteToy = favoriteToy;
-        this.age = age;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    @Override
+    public String play(){
+        return "plays with string";
     }
 
     public String getFavoriteToy() {
@@ -27,11 +21,7 @@ public class Cat {
         this.favoriteToy = favoriteToy;
     }
 
-    public int getAge() {
-        return age;
-    }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+
 }
+
